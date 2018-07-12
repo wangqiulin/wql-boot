46d116243bcd9b45f235822354418d74648b331b
wangqiulin <wangqiulin@netwisdom.cn>
2018-07-12 16:37:28 +0800

    移动配置

--- src/main/java/com/wql/boot/wqlboot/config/redis/RedissonClientConfig.java
+++ src/main/java/com/wql/boot/wqlboot/config/redis/RedissonClientConfig.java
@@ -5,31 +5,33 @@ import java.io.IOException;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 
 /**
- *
+ * redisson分布式配置
+ * 
  * @author wangqiulin
  * @date 2018年5月10日
  */
 @Configuration
 public class RedissonClientConfig {
 
-//	@Autowired
-//	private Environment env;
+	@Autowired
+	private Environment env;
 	
 	@Bean(destroyMethod = "shutdown")
 	public RedissonClient redissonClient() throws IOException {
-		/*String[] profiles = env.getActiveProfiles();
+		String[] profiles = env.getActiveProfiles();
 		String profile = "";
 		if(profiles.length > 0) {
 			profile = "-" + profiles[0];
-		}*/
+		}
 		return Redisson.create(
-			//Config.fromYAML(new ClassPathResource("config/redisson" + profile + ".yml").getInputStream())
-			Config.fromYAML(new ClassPathResource("redisson.yml").getInputStream())
+			Config.fromYAML(new ClassPathResource("config/redisson" + profile + ".yml").getInputStream())
 		);
 	}
 	
