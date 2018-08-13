d9671735d6aa0208fd1de08605e57d2d2ebe889e
wangqiulin <wangqiulin@netwisdom.cn>
2018-08-13 18:10:21 +0800

    启动时加载数据

--- src/main/java/com/wql/boot/wqlboot/WqlBootApplication.java
+++ src/main/java/com/wql/boot/wqlboot/WqlBootApplication.java
@@ -2,12 +2,22 @@ package com.wql.boot.wqlboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WqlBootApplication {
+public class WqlBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WqlBootApplication.class, args);
 	}
+	
+	/**
+	 * 将应用打车war包时， 启动类需要继承SpringBootServletInitializer， 然后重写configure()
+	 */
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WqlBootApplication.class);
+    }
 	
 }
---
+++ src/main/java/com/wql/boot/wqlboot/runner/StartUp1.java
@@ -0,0 +1,43 @@
+package com.wql.boot.wqlboot.runner;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.wql.boot.wqlboot.mapper.user.UserMapper;
+import com.wql.boot.wqlboot.model.domain.user.User;
+
+/**
+ * 启动加载数据(数据字典， 系统参数等)
+ * 
+ * @author wangqiulin
+ *
+ */
+@Component
+@Order(value = 1)
+public class StartUp1 implements CommandLineRunner {
+    
+    private static final Logger logger = LoggerFactory.getLogger(StartUp1.class);
+    
+    @Autowired
+    private SqlSessionDaoSupport sqlSessionDaoSupport;
+    
+    public void run(String... args) throws Exception {
+        logger.info(this.getClass().getName() + "启动加载数据" + args);
+        //启动时获取SqlSession
+        SqlSession sqlSession = sqlSessionDaoSupport.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> list = userMapper.selectAll();
+        for (User user : list) {
+			System.out.println(user.getDataId()+"---"+user.getUserName());
+		}
+    }
+    
+}
