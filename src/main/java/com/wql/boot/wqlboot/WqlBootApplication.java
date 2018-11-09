759df93305aa53ccf373ab366636c8be8b08e67f
wangqiulin <wangqiulin@netwisdom.cn>
2018-11-09 19:43:15 +0800

    ObjectMapper 处理时间 时区

--- src/main/java/com/wql/boot/wqlboot/WqlBootApplication.java
+++ src/main/java/com/wql/boot/wqlboot/WqlBootApplication.java
@@ -1,10 +1,24 @@
 package com.wql.boot.wqlboot;
 
+import java.io.IOException;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 @SpringBootApplication
 public class WqlBootApplication extends SpringBootServletInitializer {
@@ -12,13 +26,42 @@ public class WqlBootApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(WqlBootApplication.class, args);
 	}
-	
+
 	/**
 	 * 将应用打车war包时， 启动类需要继承SpringBootServletInitializer， 然后重写configure()
 	 */
 	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(WqlBootApplication.class);
-    }
-	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WqlBootApplication.class);
+	}
+
+	@Bean
+	@Primary
+	@ConditionalOnMissingBean(ObjectMapper.class)
+	public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+		ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+		// 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
+		// Include.Include.ALWAYS 默认
+		// Include.NON_DEFAULT 属性为默认值不序列化
+		// Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量
+		// Include.NON_NULL 属性为NULL 不序列化
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		// 允许出现特殊字符和转义符
+		objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+		// 允许出现单引号
+		objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+		// 设置时区
+		objectMapper.setTimeZone(TimeZone.getDefault());
+		// 字段保留，将null值转为""
+		objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+			@Override
+			public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+					throws IOException {
+				jsonGenerator.writeString("");
+			}
+		});
+		return objectMapper;
+	}
+
 }
--- src/main/java/com/wql/boot/wqlboot/common/mymapper/MyMapper.java
+++ src/main/java/com/wql/boot/wqlboot/common/mymapper/MyMapper.java
@@ -1,5 +1,8 @@
 package com.wql.boot.wqlboot.common.mymapper;
 
+import tk.mybatis.mapper.common.ConditionMapper;
+import tk.mybatis.mapper.common.ExampleMapper;
+import tk.mybatis.mapper.common.IdsMapper;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
@@ -7,9 +10,9 @@ import tk.mybatis.mapper.common.MySqlMapper;
  * @author wangqiulin
  * @date 2018年5月10日
  */
-public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T>, IdsMapper<T>, ConditionMapper<T>, ExampleMapper<T> {
     
-    //FIXME 特别注意，该接口不能被扫描到，否则会出错
+    //FIXME 特别注意，该接口不能被扫描到，否则会出错 
 	
 	
 }
