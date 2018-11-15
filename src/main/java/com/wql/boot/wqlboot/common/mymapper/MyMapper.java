4751da765eb542a2a3f0cd3db6db62487e520131
wangqiulin <wangqiulin@netwisdom.cn>
2018-11-15 20:29:33 +0800

    tk版本只能在3.5.3 不能升级到4

--- src/main/java/com/wql/boot/wqlboot/common/mymapper/MyMapper.java
+++ src/main/java/com/wql/boot/wqlboot/common/mymapper/MyMapper.java
@@ -1,8 +1,5 @@
 package com.wql.boot.wqlboot.common.mymapper;
 
-import tk.mybatis.mapper.common.ConditionMapper;
-import tk.mybatis.mapper.common.ExampleMapper;
-import tk.mybatis.mapper.common.IdsMapper;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
@@ -10,7 +7,7 @@ import tk.mybatis.mapper.common.MySqlMapper;
  * @author wangqiulin
  * @date 2018年5月10日
  */
-public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T>, IdsMapper<T>, ConditionMapper<T>, ExampleMapper<T> {
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
     
     //FIXME 特别注意，该接口不能被扫描到，否则会出错 
 	
