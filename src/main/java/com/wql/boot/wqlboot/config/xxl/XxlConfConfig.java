861c04d4fec39d975194dd78347db885210e1eb3
wangqiulin <wangqiulin@netwisdom.cn>
2019-04-24 15:53:49 +0800

    精简boot项目

--- src/main/java/com/wql/boot/wqlboot/config/xxl/XxlConfConfig.java
+++ src/main/java/com/wql/boot/wqlboot/config/xxl/XxlConfConfig.java
@@ -1,42 +1,44 @@
-//package com.wql.boot.wqlboot.config.xxl;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//
-///**
-// * 
-// * @author wangqiulin
-// *
-// */
-////@Configuration
-//public class XxlConfConfig {
-//	
-//    private Logger logger = LoggerFactory.getLogger(XxlConfConfig.class);
-//
-//    @Value("${xxl.conf.zkaddress}")
-//    private String zkaddress;
-//
-//    @Value("${xxl.conf.zkdigest}")
-//    private String zkdigest;
-//
-//    @Value("${xxl.conf.env}")
-//    private String env;
-//
-//    @Value("${xxl.conf.mirrorfile}")
-//    private String mirrorfile;
-//
-//    @Bean
-//    public XxlConfFactory xxlConfFactory() {
-//        XxlConfFactory xxlConf = new XxlConfFactory();
-//        xxlConf.setZkaddress(zkaddress);
-//        xxlConf.setZkdigest(zkdigest);
-//        xxlConf.setEnv(env);
-//        xxlConf.setMirrorfile(mirrorfile);
-//
-//        logger.info(">>>>>>>>>>> xxl-conf config init.");
-//        return xxlConf;
-//    }
-//
-//}
+package com.wql.boot.wqlboot.config.xxl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import com.xxl.conf.core.spring.XxlConfFactory;
+
+/**
+ * 
+ * @author wangqiulin
+ *
+ */
+//@Configuration
+public class XxlConfConfig {
+	
+	private Logger logger = LoggerFactory.getLogger(XxlConfConfig.class);
+
+    @Value("${xxl.conf.admin.address}")
+    private String adminAddress;
+
+    @Value("${xxl.conf.env}")
+    private String env;
+
+    @Value("${xxl.conf.access.token}")
+    private String accessToken;
+
+    @Value("${xxl.conf.mirrorfile}")
+    private String mirrorfile;
+
+    
+    @Bean
+    public XxlConfFactory xxlConfFactory() {
+        XxlConfFactory xxlConf = new XxlConfFactory();
+        xxlConf.setAdminAddress(adminAddress);
+        xxlConf.setEnv(env);
+        xxlConf.setAccessToken(accessToken);
+        xxlConf.setMirrorfile(mirrorfile);
+        logger.info(">>>>>>>>>>> xxl-conf config init.");
+        return xxlConf;
+    }
+    
+}
--- src/main/java/com/wql/boot/wqlboot/config/xxl/XxlJobConfig.java
+++ src/main/java/com/wql/boot/wqlboot/config/xxl/XxlJobConfig.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
@@ -14,8 +13,7 @@ import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
  * @author wangqiulin
  * @date 2018年5月12日
  */
-//@Configuration
-@ComponentScan(basePackages = "com.wql.boot.wqlboot.job")
+@Configuration
 public class XxlJobConfig {
 	
     private Logger logger = LoggerFactory.getLogger(XxlJobConfig.class);
@@ -44,15 +42,32 @@ public class XxlJobConfig {
     @Bean(initMethod = "start", destroyMethod = "destroy")
     public XxlJobSpringExecutor xxlJobExecutor() {
         logger.info(">>>>>>>>>>> xxl-job config init.");
-        XxlJobSpringExecutor xxlJobExecutor = new XxlJobSpringExecutor();
-        xxlJobExecutor.setAdminAddresses(adminAddresses);
-        xxlJobExecutor.setAppName(appName);
-        xxlJobExecutor.setIp(ip);
-        xxlJobExecutor.setPort(port);
-        xxlJobExecutor.setAccessToken(accessToken);
-        xxlJobExecutor.setLogPath(logPath);
-        xxlJobExecutor.setLogRetentionDays(logRetentionDays);
-        return xxlJobExecutor;
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppName(appName);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+        return xxlJobSpringExecutor;
     }
 
+    /**
+     * 针对多网卡、容器内部署等情况，可借助 "spring-cloud-commons" 提供的 "InetUtils" 组件灵活定制注册IP；
+     *
+     *      1、引入依赖：
+     *          <dependency>
+     *             <groupId>org.springframework.cloud</groupId>
+     *             <artifactId>spring-cloud-commons</artifactId>
+     *             <version>${version}</version>
+     *         </dependency>
+     *
+     *      2、配置文件，或者容器启动变量
+     *          spring.cloud.inetutils.preferred-networks: 'xxx.xxx.xxx.'
+     *
+     *      3、获取IP
+     *          String ip_ = inetUtils.findFirstNonLoopbackHostInfo().getIpAddress();
+     */
+    
 }
