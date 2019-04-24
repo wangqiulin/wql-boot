392eebe270c9f8dd2f75751771e73d1dcefa6048
wangqiulin <wangqiulin@netwisdom.cn>
2019-04-24 16:01:03 +0800

    nothing

--- src/main/java/com/wql/boot/wqlboot/config/rabbitmq/AmqpConfig.java
+++ src/main/java/com/wql/boot/wqlboot/config/rabbitmq/AmqpConfig.java
@@ -15,7 +15,6 @@ import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import com.rabbitmq.client.Channel;
